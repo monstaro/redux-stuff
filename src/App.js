ef87d5963d8b23fb30dbfc79d7eaae9129de95ad
monstaro <codysmith2814@gmail.com>
2021-02-17 13:34:16 -0700

    Add edit post function

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ import {
   Route,
   Redirect,
 } from 'react-router-dom'
-
+import { PostsList } from './features/posts/PostList';
+import { AddPostForm } from './features/posts/AddPostForm';
+import { SinglePost } from './features/posts/SinglePost';
+import { EditPost } from './features/posts/EditPost';
 import { Navbar } from './app/Navbar'
 
 function App() {
@@ -18,11 +21,14 @@ function App() {
             exact
             path="/"
             render={() => (
-              <section>
-                <h2>Welcome to the Redux Essentials example app!</h2>
-              </section>
+              <>
+              <AddPostForm />
+              <PostsList />
+              </>
             )}
           />
+          <Route exact path="/editPost/:postId" component={EditPost} />
+          <Route exact path="/posts/:postId" component={SinglePost} />
           <Redirect to="/" />
         </Switch>
       </div>
--- src/app/Navbar.js
+++ src/app/Navbar.js
@@ -1,13 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const Navbar = () => {
   return (
     <nav>
       <section>
-        <h1>Redux Essentials Example</h1>
+        <h1>Redux</h1>
 
         <div className="navContent">
-          <div className="navLinks"></div>
+          <div className="navLinks">
+            <Link to="/">All Posts</Link>
+          </div>
         </div>
       </section>
     </nav>
